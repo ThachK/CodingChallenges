c9938107a8cd04ff17abc37961c72301d6a9f341
Kimmy Thach <52873824+ThachK@users.noreply.github.com>
2023-03-14 09:32:35 -0500

    Create addition.js

---
+++ addition.js
@@ -0,0 +1,10 @@
+const addition = (num1, num2) => {
+    let sum = num1 + num2;
+    return sum;
+}
+
+const num1 = parseInt(prompt("Enter the first number to add: "));
+
+const num2 = parseInt(prompt("Enter the second number to add: "));
+
+console.log(addition(num1, num2));
